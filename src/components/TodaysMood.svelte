9a76acb313d89bd6840729614b8cf272f30a820d
FelixAL07 <lerdahl07@gmail.com>
2025-12-04 17:07:21 +0100

    feat: initialize mood variable and add conditional rendering for mood display in TodaysMood component

--- src/components/TodaysMood.svelte
+++ src/components/TodaysMood.svelte
@@ -1,12 +1,20 @@
 <script lang="ts">
     import { currentMood } from "../services/moodService";
     import { todaysMood } from "../stores/store";
-    $: mood = currentMood($todaysMood)
+    let mood: string = "";
+    $: mood = currentMood($todaysMood);
 </script>
 
 <div class="flex items-center justify-center gap-1 sm:gap-2 flex-col">
     <h1 class="text-2xl font-bold">Kontorets humør i dag!</h1>
-    <span class="text-[3rem] sm:text-[4rem] transition-all duration-300 ease-out hover:scale-110 hover:opacity-90 cursor-pointer flex items-center justify-center">
-        {mood}
-    </span>
+   <!-- Smaller text if it isnt any votes yet --> 
+    {#if mood === "Ingen har avgitt stemme for idag"}
+        <span class="text-[1rem] sm:text-[2rem] transition-all duration-300 ease-out hover:scale-110 hover:opacity-90 cursor-pointer flex items-center justify-center">
+            {mood}
+        </span>
+    {:else}
+        <span class="text-[3rem] sm:text-[4rem] transition-all duration-300 ease-out hover:scale-110 hover:opacity-90 cursor-pointer flex items-center justify-center">
+            {mood}
+        </span>
+    {/if}
 </div>
